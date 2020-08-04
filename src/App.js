35fcd8dcb6acf0a3c9861cc91a7fd310f9621536
George Boyar <gs.boyar@gmail.com>
2020-08-04 17:40:50 -0400

    through comic 65
    through comic 65

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import RedditIcon from "./icons/Reddit.js";
 import InstagramIcon from "./icons/Instagram.js";
 import ReactGA from 'react-ga';
 
-const MAX_IMAGES = 52;
+const MAX_IMAGES = 65;
 ReactGA.initialize('UA-138331555-1');
 
 function IndividualComic() {
