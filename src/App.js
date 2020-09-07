77378cd6956fa8b93f150c152289339120bc0f1e
George Boyar <gs.boyar@gmail.com>
2020-09-07 10:11:37 -0400

    up to comic 69

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import RedditIcon from "./icons/Reddit.js";
 import InstagramIcon from "./icons/Instagram.js";
 import ReactGA from 'react-ga';
 
-const MAX_IMAGES = 65;
+const MAX_IMAGES = 69;
 ReactGA.initialize('UA-138331555-1');
 
 function IndividualComic() {
